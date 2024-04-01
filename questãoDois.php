840a0a205876c6da8c000013c37a11fc0b2ec8fe
Samuelvmt <145499760+Samuelvmt@users.noreply.github.com>
2024-04-01 20:17:49 -0300

    Add files via upload

---
+++ questãoDois.php
@@ -0,0 +1,25 @@
+<?php
+$alienigena1=10;
+$marciano2=0;
+
+
+
+
+function sapato44($alienigena1){
+
+
+if ($alienigena %10 == 0  ){
+
+  echo(" o valor é divisivel por 10 $alienigena1" );
+
+}
+
+
+
+}
+
+
+
+
+
+?>
---
+++ questãoUm.php
@@ -0,0 +1,34 @@
+<?php
+$soap=2;
+$xmp=3;
+$soma=0;
+
+
+$soma=$xmp+$soap;
+$resp=$soma;
+
+if ($resp>20){
+
+$resp=$resp+8;
+}
+
+
+
+else if($resp<=20) {
+
+$resp=$resp-5;
+
+}
+
+
+echo ("o resultado é $resp");
+
+//Samuel Torres rm:23667
+
+//Breno Souza rm:23725
+?>
+
+
+
+
+
